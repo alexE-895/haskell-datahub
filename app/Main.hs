6863e8b120bfe3cf4ed49b46b5a40e8e64125a47
Alex <kok20112@gmail.com>
2026-08-27 18:59:59 +0300

    refactor: introduce DataHub application module

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-module Main (main) where
+﻿module Main
+  ( main
+  ) where
 
-import qualified MyLib (someFunc)
+import DataHub.App (runApp)
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = runApp
---
+++ src/DataHub/App.hs
@@ -0,0 +1,6 @@
+﻿module DataHub.App
+  ( runApp
+  ) where
+
+runApp :: IO ()
+runApp = putStrLn "Haskell DataHub starting..."
